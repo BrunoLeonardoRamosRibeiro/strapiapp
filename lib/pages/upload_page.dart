790f46350041771abe1482c92a61c78f1cc66782
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2020-11-09 12:08:29 -0300

    Versão 09 de novembro de 2020

--- lib/pages/upload_page.dart
+++ lib/pages/upload_page.dart
@@ -8,6 +8,7 @@ import 'package:http_parser/http_parser.dart';
 import 'package:path/path.dart';
 import 'package:async/async.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:strapiapp/shared/models/image.dart' as img;
 
 class UploadPage extends StatefulWidget {
   @override
@@ -45,22 +46,24 @@ class _UploadPageState extends State<UploadPage> {
                   width: 300,
                   child: _image == null
                       ? Image.asset(
-                    "assets/images/camera.png",
-                    width: 140,
-                  )
+                          "assets/images/camera.png",
+                          width: 140,
+                        )
                       : Image.file(_image),
                 ),
               ),
               Visibility(
                 visible: upload == true,
-                child: Center(child: CircularProgressIndicator(),),
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
               ),
             ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async{
+        onPressed: () async {
           setState(() {
             upload = true;
           });
@@ -76,22 +79,27 @@ class _UploadPageState extends State<UploadPage> {
   }
 
   Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    //var image = await
+    ImagePicker.pickImage(source: ImageSource.camera).then((value) async {
+      if (value != null) {
+        setState(() {
+          _image = value;
+          // Upload da imagem
+        });
 
-    setState(() {
-      _image = image;
-      // Upload da imagem
+        await uploadImage2(value.path, value.readAsBytesSync());
+      }
     });
-
-    await uploadImage(image.path, image.readAsBytesSync());
   }
 
-  Future uploadImage(String imageFilePath,
-      Uint8List imageBytes,) async {
+  Future uploadImage(
+    String imageFilePath,
+    Uint8List imageBytes,
+  ) async {
     String url = "https://superfan-strapi-api.herokuapp.com" + "/upload";
     PickedFile imageFile = PickedFile(imageFilePath);
     var stream =
-    new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+        new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
 
     var uri = Uri.parse(url);
     int length = imageBytes.length;
@@ -106,5 +114,38 @@ class _UploadPageState extends State<UploadPage> {
     response.stream.transform(utf8.decoder).listen((value) {
       print(value);
     });
+  }
+
+  Future uploadImage2(
+    String imageFilePath,
+    Uint8List imageBytes,
+  ) async {
+    String url = "https://superfan-strapi-api.herokuapp.com" + "/upload";
+    PickedFile imageFile = PickedFile(imageFilePath);
+    var stream =
+        new http.ByteStream(DelegatingStream.typed(imageFile.openRead()));
+
+    var uri = Uri.parse(url);
+    int length = imageBytes.length;
+    var request = new http.MultipartRequest("POST", uri);
+    var multipartFile = new http.MultipartFile('files', stream, length,
+        filename: basename(imageFile.path),
+        contentType: MediaType('image', 'jpg'));
+
+    List<img.Image> images;
+
+    request.files.add(multipartFile);
+    var response = await request.send();
+    print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      response.stream.transform(utf8.decoder).listen((value) {
+        print(value);
+        Iterable lista = json.decode(value);
+        images = lista.map((model) => img.Image.fromJson(model)).toList();
+        print(images[0].name);
+        print(images[0].url);
+      });
+    }
   }
 }
